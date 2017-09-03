5ace67d6d0943704d54e6ca935360d9e41cd0589
ev-smirnov <percerez@gmail.com>
2017-09-03 14:22:17 +0300

    add simple saga with test

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,12 +2,17 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { routerMiddleware } from 'react-router-redux'
+import creacteSagaMiddleware from 'redux-saga';
 import history from '../history'
 import reducer from './reducer'
+import { saga } from '../ducks/people';
 
-const enhancer = applyMiddleware(routerMiddleware(history), thunk, logger)
+const sagaMiddleware = creacteSagaMiddleware();
+const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger)
 
 const store = createStore(reducer, enhancer)
 window.store = store
+
+sagaMiddleware.run(saga);
 
 export default store
